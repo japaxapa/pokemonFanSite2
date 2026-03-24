604661e126592d8d872b774e9e956b56ead8b242
Mario Kawakita <mario.jhk@gmail.com>
2026-03-24 15:45:11 -0300

    feat: accordion | filter acordion

--- src/components/pokedex/AdvFilter.tsx
+++ src/components/pokedex/AdvFilter.tsx
@@ -1,18 +1,14 @@
-import { GENERATIONS_PARAMS } from '#/constants/consts'
-import { Form, FormField, FormLabel } from '@radix-ui/react-form'
 import {
   Box,
   Button,
   Card,
-  Dialog,
   Flex,
-  RadioCards,
-  Text,
 } from '@radix-ui/themes'
-import FilterIcon from '../icons/FilterIcon'
 import FilterOffIcon from '../icons/FilterOffIcon'
 import { useState } from 'react'
+import FilterModal from './FilterModal'
 
+// TODO check if selectedGeneration should be a global state
 interface IADVFilter {
   selectedGeneration: number | null
   setSelectedGeneration: (generation: number | null) => void
@@ -36,7 +32,7 @@ export default function AdvancedFilter({
     // GEN Filter
     const generationValue = formData.get('generation') as string | null
     const generationIndex =
-      generationValue && (generationValue !== '' && generationValue !== '9')
+      generationValue && generationValue !== '' && generationValue !== '9'
         ? parseInt(generationValue, 10)
         : null
 
@@ -50,65 +46,10 @@ export default function AdvancedFilter({
     <Card size={'2'} mb={'3'}>
       <Box width={'100%'} px={'5'}>
         <Flex gap={'4'}>
-          <Dialog.Root>
-            <Dialog.Trigger>
-              <Button>
-                <FilterIcon /> Filter
-              </Button>
-            </Dialog.Trigger>
-            <Dialog.Content
-              aria-description="Filters for pokedex query"
-              aria-describedby="filters"
-            >
-              <Dialog.Title>
-                <Text weight={'bold'}>Filters</Text>
-              </Dialog.Title>
-              <Dialog.Description>
-                <Text weight={'medium'} size={'3'}>
-                  Select Filters to apply to the pokedex
-                </Text>
-              </Dialog.Description>
-              <Card my={'2'} size={'2'}>
-                <Flex direction={'column'} gap={'4'}>
-                  <Form onSubmit={handleSubmit}>
-                    <FormField name="generationWrapper">
-                      <Flex direction={'column'} gap={'4'}>
-                        <FormLabel>
-                          <Text weight={'medium'}>Generation</Text>
-                        </FormLabel>
-                        <RadioCards.Root
-                          mt={'1'}
-                          defaultValue={
-                            selectedGeneration
-                              ? selectedGeneration.toString()
-                              : '9'
-                          }
-                          name="generation"
-                        >
-                          {GENERATIONS_PARAMS.map((GENERATION, idx) => (
-                            <RadioCards.Item value={idx.toString()} key={idx}>
-                              <Text>{GENERATION.name}</Text>
-                            </RadioCards.Item>
-                          ))}
-                        </RadioCards.Root>
-                      </Flex>
-                    </FormField>
-
-                    <Flex gap="3" mt="4" justify="end">
-                      <Dialog.Close>
-                        <Button variant="soft" color="gray">
-                          Cancel
-                        </Button>
-                      </Dialog.Close>
-                      <Dialog.Close>
-                        <Button type="submit">Apply</Button>
-                      </Dialog.Close>
-                    </Flex>
-                  </Form>
-                </Flex>
-              </Card>
-            </Dialog.Content>
-          </Dialog.Root>
+          <FilterModal
+            handleSubmit={handleSubmit}
+            selectedGeneration={selectedGeneration}
+          />
           {hasFilter && (
             <Button onClick={clearFilters}>
               <FilterOffIcon /> Remove Filters
